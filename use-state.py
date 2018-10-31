be402f1d0490b15fd1df56d11e83f8bafc24cbcd
With the light, as low as dust <43346455+haitanghuadeng@users.noreply.github.com>
2018-10-31 16:15:55 +0800

    Add files via upload

---
+++ use-state.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# Author:@haitanghuadeng
+
+
+#
+
+print("Please to terminal\nimport statement: ___ pip install pyecharts\n")
+print("This is very vital! The magic of color comes out when you have ___ 'pyecharts'.")
+
