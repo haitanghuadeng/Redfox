e32080ec361db34ed968d52e8cd0403885849cfb
With the light, as low as dust <43346455+haitanghuadeng@users.noreply.github.com>
2018-11-03 13:44:24 +0800

    Delete use-state.py

--- use-state.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-# Author:@haitanghuadeng
-
-
-#
-
-print("Please to terminal\nimport statement: ___ pip install pyecharts\n")
-print("This is very vital! The magic of color comes out when you have ___ 'pyecharts'.")
-
