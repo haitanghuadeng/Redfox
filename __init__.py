d819297c4f4a7afadcf50e3da8b7754e01604fb7
With the light, as low as dust <43346455+haitanghuadeng@users.noreply.github.com>
2018-11-21 02:10:20 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,35 +1,36 @@
 # -*- coding:utf-8 -*-
 
 
-from Redfox.function._version import __version__, __author__
+from Redfox.function.charts import *
 
 # charts
-from charts.bar import Bar
-from charts.bar3D import Bar3D
-from charts.boxplot import Boxplot
-from charts.effectscatter import EffectScatter
-from charts.funnel import Funnel
-from charts.gauge import Gauge
-from charts.geo import Geo
-from charts.geolines import Geolines
-from charts.graph import Graph
-from charts.heatmap import Heatamp
-from charts.kline import Kline
-from charts.candlestick import Candlestick
-from charts.line import Line
-from charts.line3d import Line3D
-from charts.liquid import Liquid
-from charts.map import Map
-from charts.parallel import Parallel
-from charts.pie import Pie
-from charts.polar import Polar
-from charts.radar import Radar
-from charts.sankey import Sankey
-from charts.scatter import Scatter
-from charts.surface import Surface
-from charts.themerive import Themerive
-from charts.tree import Tree
-from charts.treemap import TreeMap
-from charts.wordcloud import WordCloud
+from Redfox.function.charts import bar
+from Redfox.function.charts.bar3D import Bar3D
+from Redfox.function.charts.boxplot import Boxplot
+from Redfox.function.charts.effectscatter import EffectScatter
+from Redfox.function.charts.funnel import Funnel
+from Redfox.function.charts.gauge import Gauge
+from Redfox.function.charts.geo import Geo
+from Redfox.function.charts.geolines import Geolines
+from Redfox.function.charts.graph import Graph
+from Redfox.function.charts.heatmap import Heatamp
+from Redfox.function.charts.kline import Kline
+from Redfox.function.charts.candlestick import Candlestick
+from Redfox.function.charts.line import Line
+from Redfox.function.charts.line3d import Line3D
+from Redfox.function.charts.liquid import Liquid
+from Redfox.function.charts.map import Map
+from Redfox.function.charts.parallel import Parallel
+from Redfox.function.charts.pie import Pie
+from Redfox.function.charts.polar import Polar
+from Redfox.function.charts.radar import Radar
+from Redfox.function.charts.sankey import Sankey
+from Redfox.function.charts.scatter import Scatter
+from Redfox.function.charts.scatter3D import Scatter3D
+from Redfox.function.charts.surface3D import Surface3D
+from Redfox.function.charts.themerive import Themerive
+from Redfox.function.charts.tree import Tree
+from Redfox.function.charts.treemap import TreeMap
+from Redfox.function.charts.wordcloud import WordCloud
 
 name = "Pyecharts-RedFox"
